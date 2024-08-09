904b333f7b9d4de1d2c590f66dcd2e4c10cd3c30
Daniel Taylor <dtaylo24@gmail.gmu>
2024-08-08 22:04:26 -0500

    Remove polyfill

--- physics/generator.scm
+++ physics/generator.scm
@@ -961,14 +961,14 @@
                 (string-append "<title>Physics with Danno: " (lesson-title lesson-number) "</title>")
                 (string-append "<title>Chapters</title>")
                 )
-            "<script src=\"https://polyfill.io/v3/polyfill.min.js?features=es6\"></script>"
-            "<script id=\"MathJax-script\" async src=\"https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js\"></script>"
             "</head>"
             "<body style=\"margin-left:auto; margin-right:auto; width: 80ch; font-size: 1.125rem\">"
             "<h1><a href=\"/physics/\">Physics with Danno</a></h1>")))
 (define physics-with-danno-html-footer
     (string-append
         "<p>&copy; by Daniel Taylor</p>"
+        "<script id=\"MathJax-script\" async src=\"https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js\"></script>"
+        "<script id=\"MathJax-script\" async src=\"/3rd-party/mathjax/tex-mml-chtml.js\">"
         "</body></html>"))
 
 (define chapter-bar
